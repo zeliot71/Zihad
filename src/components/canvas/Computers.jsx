5f228d57f2babf021ef2acdb70e37311a735a2f8
Md. Zihad <50814580+zeliot71@users.noreply.github.com>
2025-08-14 00:18:26 +0600

    Lower computer 3D model position

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -45,7 +45,7 @@ const Computers = () => {
       <primitive
         object={computer.scene}
         scale={0.75} // Keep scale normal for desktop
-        position={[0, -3.25, -1.5]}
+        position={[0, -3.27, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
