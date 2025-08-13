10dccdf4b90f1f18c8881bfe0cabaf212c370706
Md. Zihad <50814580+zeliot71@users.noreply.github.com>
2025-08-14 00:13:18 +0600

    Change name from Huzaif to Zihad

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
 
         <div className="mt-28 md:mt-5 lg:5">
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Huzaif</span>
+            Hi, I'm <span className='text-[#915EFF]'>Zihad</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop modern, user <br/>
