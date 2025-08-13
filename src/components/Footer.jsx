6b869df29718f915fd4bf801097df7de95733ae5
Md. Zihad <50814580+zeliot71@users.noreply.github.com>
2025-08-14 00:05:55 +0600

    Fix missing FaFacebook import

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaInstagram, FaXTwitter, FaLinkedin } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
   return (
